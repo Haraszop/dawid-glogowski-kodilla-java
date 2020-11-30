a20f2c86b17aae08403ce1699d29a9f150974e73
dawid glogowski <dawid.glogowski3@gmail.com>
2020-11-30 20:31:24 +0100

    6.4 Added ShapeCollector adn Tests

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,33 @@
+package com.kodilla.testing.shape;
+
+import java.util.*;
+
+public class ShapeCollector {
+    private List<Shape> figures = new ArrayList<Shape>();
+
+    public List<Shape> getFigures() {
+        return figures;
+    }
+
+    public void addFigure(Shape shape) {
+        figures.add(shape);
+    }
+    public boolean removeFigure(Shape shape) {
+        boolean result = false;
+        if (figures.contains(shape)) {
+            figures.remove(shape);
+            result = true;
+        }
+        return result;
+    }
+    public Shape getFigure(int n) {
+        return figures.get(n);
+    }
+    public String showFigures() {
+        String show = "";
+        for (Shape fig: figures){
+            show = show + (fig.getShapeName() + " " + fig.getField()) + "\n";
+        }
+        return show;
+    }
+}
